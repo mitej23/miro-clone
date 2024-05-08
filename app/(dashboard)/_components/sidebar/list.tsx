cbc2bc83fada1b453451f1d081e6b5f73fd059ab
mitej23 <mitejmadan@gmail.com>
2024-05-09 03:43:48 +0530

    feat: added sidebar and initial setup

---
+++ app/(dashboard)/_components/sidebar/list.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useOrganizationList } from "@clerk/nextjs";
+
+import { Item } from "./item";
+
+export function List() {
+  const { userMemberships } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  });
+
+  console.log(userMemberships)
+
+  if (userMemberships.data?.length === 0) return null;
+
+  
+
+  return (
+    <ul className="space-y-4">
+      {userMemberships.data?.map((membership) => {
+        
+        return (
+          <Item
+            id={membership.organization.id}
+            key={membership.organization.id}
+            imageUrl={membership.organization.imageUrl}
+            name={membership.organization.name}
+          />
+        );
+      })}
+    </ul>
+  );
+}
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://organic-pony-45.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
